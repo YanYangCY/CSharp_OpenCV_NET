c1620af3f810163bf70208d5a51f361a3b1ebf8d
阳 严 <2582960451@qq.com>
2025-08-27 15:39:11 +0800

    添加项目文件。

---
+++ CSharp_OpenCV_NET/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CSharp_OpenCV_NET.View;
+using CSharp_OpenCV_NET.ViewModel;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CSharp_OpenCV_NET
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+
+}
---
+++ CSharp_OpenCV_NET/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using CSharp_OpenCV_NET.Model;
+
+namespace CSharp_OpenCV_NET.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _newTitle = "";
+
+        public ObservableCollection<Todo> Items { get; } = new();
+
+        [RelayCommand]
+        private void Add()
+        {
+            if (string.IsNullOrWhiteSpace(NewTitle)) 
+                return;
+            Items.Add(new Todo { Title = NewTitle });
+            NewTitle = string.Empty;
+        }
+
+        [RelayCommand]
+        private void Remove(Todo item) => Items.Remove(item);
+    }
+}
